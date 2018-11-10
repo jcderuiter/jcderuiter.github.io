48b1e879c20ff8b90ff19c2f1e0543f86d6b2848
jos <jcderuiter@mail.com>
2018-11-10 21:19:35 +0100

    Update jcr.js

--- jcr.js
+++ jcr.js
@@ -1,16 +1,7 @@
 'use strict';
 const jcr_js_object = {};
-try {
 document.addEventListener('DOMContentLoaded', () => jcr_js_object.onload(), false);
-} catch(e) { console.log(`oops: ${e}`) }
    
-//onload = (function(loadbefore)
-//{  return function()
-//   {  if (loadbefore) loadbefore();
-//      jcr_js_object.onload();
-//   }
-//})(onload);
-
 (function(api)
 {
    api.onload = function()
